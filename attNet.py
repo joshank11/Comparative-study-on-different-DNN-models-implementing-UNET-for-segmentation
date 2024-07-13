df2b79c8dcd743ca17bc554fbe309ce3b174ed63
joshank11 <127469631+joshank11@users.noreply.github.com>
2024-07-13 15:26:00 +0530

    Add files via upload

---
+++ attNet.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import cv2
+import numpy as np
+import skimage.io
+from matplotlib import pyplot as plt
+from patchify import patchify
+from PIL import Image
+np.random.seed(0) 
+
+
+#CLAHE
+def clahe_equalized(imgs):    
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))    
+    imgs_equalized = clahe.apply(imgs)
+    return imgs_equalized
+
+
+path1 = '../Data/train/image/' #training images directory
+path2 = '../Data/train/mask/' #training masks directory
+
+image_dataset = []
+mask_dataset = [] 
+
+patch_size = 256
+
+images = sorted(os.listdir(path1)) 
+for i, image_name in enumerate(images):  
+   if image_name.endswith(".png"):                   
+       image = skimage.io.imread(path1+"/"+image_name)  #Read image
+       image = image[:,:,1] #selecting green channel
+       image = clahe_equalized(image) #applying CLAHE
+       SIZE_X = (image.shape[1]//patch_size)*patch_size #getting size multiple of patch size
+       SIZE_Y = (image.shape[0]//patch_size)*patch_size #getting size multiple of patch size
+       image = Image.fromarray(image)        
+       image = image.resize((SIZE_X, SIZE_Y)) #resize image       
+       image = np.array(image) 
+       patches_img = patchify(image, (patch_size, patch_size), step=patch_size)  #create patches(patch_sizexpatch_sizex1)
+            
+       for i in range(patches_img.shape[0]):
+           for j in range(patches_img.shape[1]):                        
+               single_patch_img = patches_img[i,j,:,:]                 
+               single_patch_img = (single_patch_img.astype('float32')) / 255.                    
+               image_dataset.append(single_patch_img)
+
+masks = sorted(os.listdir(path2))  
+for i, mask_name in enumerate(masks):  
+    if mask_name.endswith(".png"):                  
+        mask = skimage.io.imread(path2+"/"+mask_name)   #Read masks
+        SIZE_X = (mask.shape[1]//patch_size)*patch_size #getting size multiple of patch size
+        SIZE_Y = (mask.shape[0]//patch_size)*patch_size #getting size multiple of patch size
+        mask = Image.fromarray(mask)        
+        mask = mask.resize((SIZE_X, SIZE_Y))  #resize image
+        mask = np.array(mask)
+        patches_mask = patchify(mask, (patch_size, patch_size), step=patch_size)  #create patches(patch_sizexpatch_sizex1)
+            
+        for i in range(patches_mask.shape[0]):
+            for j in range(patches_mask.shape[1]):                            
+                single_patch_mask = patches_mask[i,j,:,:]
+                single_patch_mask = (single_patch_mask.astype('float32'))/255. 
+                mask_dataset.append(single_patch_mask) 
+ 
+image_dataset = np.array(image_dataset)
+mask_dataset =  np.array(mask_dataset)
+image_dataset = np.expand_dims(image_dataset,axis=-1)
+mask_dataset =  np.expand_dims(mask_dataset,axis=-1)
+
+
+
+# In[2]:
+
+
+#importing models
+# from model import unetmodel, residualunet, attentionunet, attention_residualunet
+from model import unetmodel, residualunet, attentionunet
+
+from tensorflow.keras.optimizers import Adam
+from evaluation_metrics import IoU_coef,IoU_loss
+
+IMG_HEIGHT = patch_size
+IMG_WIDTH = patch_size
+IMG_CHANNELS = 1
+input_shape = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+optimizer = Adam(learning_rate=0.001)
+
+model = attentionunet(input_shape)
+model.compile(optimizer = optimizer, loss= IoU_loss, metrics= ['accuracy', IoU_coef])
+
+
+#splitting data into 70-30 ratio to validate training performance
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(image_dataset, mask_dataset, test_size=0.3, random_state=0)
+
+#train model
+history = model.fit(x_train, y_train, 
+                    verbose=1,
+                    batch_size = 16,
+                    validation_data=(x_test, y_test ), 
+                    shuffle=False,
+                    epochs=150)
+
+
+# In[3]:
+
+
+model.save('./retina_attUnet.hdf5')
+
+
+# In[5]:
+
+
+print(history.history)
+
+
+# In[4]:
+
+
+#training-validation loss curve
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.figure(figsize=(7,5))
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'y', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+# plt.show()
+plt.savefig('loss_curve.png')
+
+
+#training-validation accuracy curve
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+plt.figure(figsize=(7,5))
+plt.plot(epochs, acc, 'r', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'y', label='Validation Accuracy')
+plt.title('Training and validation accuracies')
+plt.xlabel('Epochs')
+plt.ylabel('IoU')
+plt.legend()
+# plt.show()
+plt.savefig('accuracy_curve.png')
+
+#training-validation IoU curve
+iou_coef = history.history['io_u_coef']
+val_iou_coef = history.history['val_io_u_coef']
+plt.figure(figsize=(7,5))
+plt.plot(epochs, iou_coef, 'r', label='Training IoU')
+plt.plot(epochs, val_iou_coef, 'y', label='Validation IoU')
+plt.title('Training and validation IoU coefficients')
+plt.xlabel('Epochs')
+plt.ylabel('IoU')
+plt.legend()
+# plt.show()
+plt.savefig('validation_curve.png')
+
---
+++ unet_test.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import cv2
+import numpy as np
+import skimage.io
+from matplotlib import pyplot as plt
+from patchify import patchify, unpatchify
+np.random.seed(0)
+
+# CLAHE
+def clahe_equalized(imgs):    
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))    
+    imgs_equalized = clahe.apply(imgs)
+    return imgs_equalized
+
+patch_size = 256
+
+#loading model architectures
+# from model import unetmodel, residualunet, attentionunet, attention_residualunet
+from model import unetmodel, residualunet, attentionunet
+
+from tensorflow.keras.optimizers import Adam
+from evaluation_metrics import IoU_coef,IoU_loss
+
+IMG_HEIGHT = patch_size
+IMG_WIDTH = patch_size
+IMG_CHANNELS = 1
+
+input_shape = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+model = unetmodel(input_shape) #/residualunet(input_shape)/attentionunet(input_shape)/attention_residualunet(input_shape) 
+
+optimizer = Adam(learning_rate=0.001)
+
+model.compile(optimizer = optimizer, loss= IoU_loss, metrics= ['accuracy', IoU_coef])
+model.load_weights('./retina_Unet.hdf5') #loading weights
+
+
+path1 = '../Data/test/image/'    #test dataset images directory path
+path2 = '../Data/test/mask/'     #test dataset mask directory path
+
+
+from sklearn.metrics import jaccard_score,confusion_matrix
+
+testimg = []
+ground_truth = []
+prediction = []
+global_IoU = []
+global_accuracy = []
+
+testimages = sorted(os.listdir(path1))
+testmasks =  sorted(os.listdir(path2))
+
+for idx, image_name in enumerate(testimages):  
+   if image_name.endswith(".png"):  
+      predicted_patches = []
+      test_img = skimage.io.imread(path1+"/"+image_name)
+     
+      test = test_img[:,:,1] #selecting green channel
+      test = clahe_equalized(test) #applying CLAHE
+      SIZE_X = (test_img.shape[1]//patch_size)*patch_size #getting size multiple of patch size
+      SIZE_Y = (test_img.shape[0]//patch_size)*patch_size #getting size multiple of patch size
+      test = cv2.resize(test, (SIZE_X, SIZE_Y))
+      testimg.append(test)           
+      test = np.array(test)
+
+      patches = patchify(test, (patch_size, patch_size), step=patch_size) #create patches(patch_sizexpatch_sizex1)
+
+      for i in range(patches.shape[0]):
+                for j in range(patches.shape[1]):
+                  single_patch = patches[i,j,:,:]
+                  single_patch_norm = (single_patch.astype('float32')) / 255.
+                  single_patch_norm = np.expand_dims(np.array(single_patch_norm), axis=-1)
+                  single_patch_input = np.expand_dims(single_patch_norm, 0)
+                  single_patch_prediction = (model.predict(single_patch_input)[0,:,:,0] > 0.5).astype(np.uint8) #predict on single patch
+                  predicted_patches.append(single_patch_prediction)
+      predicted_patches = np.array(predicted_patches)
+      predicted_patches_reshaped = np.reshape(predicted_patches, (patches.shape[0], patches.shape[1], patch_size,patch_size) )
+      reconstructed_image = unpatchify(predicted_patches_reshaped, test.shape) #join patches to form whole img
+      prediction.append(reconstructed_image) 
+
+      groundtruth=[]
+      groundtruth = skimage.io.imread(path2+'/'+testmasks[idx]) #reading mask of the test img      
+      SIZE_X = (groundtruth.shape[1]//patch_size)*patch_size 
+      SIZE_Y = (groundtruth.shape[0]//patch_size)*patch_size  
+      groundtruth = cv2.resize(groundtruth, (SIZE_X, SIZE_Y))  
+      ground_truth.append(groundtruth)
+
+      y_true = groundtruth
+      y_pred = reconstructed_image
+            
+            
+
+      plt.figure(figsize=(20, 18))
+      plt.subplot(231)
+      plt.title('Test Image')
+      plt.xticks([])
+      plt.yticks([])
+      plt.imshow(test)
+      plt.subplot(232)
+      plt.title('Ground Truth')
+      plt.xticks([])
+      plt.yticks([])
+      plt.imshow(y_true,cmap='gray')
+      plt.subplot(233)
+      plt.title('Prediction')
+      plt.xticks([])
+      plt.yticks([])
+      plt.imshow(y_pred,cmap='gray')
+      plt.savefig(f'./test-output/{image_name}')
+
+
+
+      labels = [0, 1]
+      IoU = []
+      for label in labels:
+          jaccard = jaccard_score(y_pred.flatten(),y_true.flatten(), pos_label=label, average='weighted')
+          IoU.append(jaccard)
+      IoU = np.mean(IoU) #jacard/IoU of single image
+      global_IoU.append(IoU)
+
+      cm=[]
+      accuracy = []
+      cm = confusion_matrix(y_true.flatten(),y_pred.flatten(), labels=[0, 1])
+      accuracy = (cm[0,0]+cm[1,1])/(cm[0,0]+cm[0,1]+cm[1,0]+cm[1,1]) #accuracy of single image
+      global_accuracy.append(accuracy)
+
+
+avg_acc =  np.mean(global_accuracy)
+mean_IoU = np.mean(global_IoU)
+
+print('Average accuracy is',avg_acc)
+print('mean IoU is',mean_IoU)
+
+
